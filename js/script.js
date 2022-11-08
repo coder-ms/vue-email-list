00a9fdb8a95e078a11646032bd22e4ffb43d798b
coder-ms <marco.sbrascia@gmail.com>
2022-11-08 17:16:33 +0200

    inserimento mail

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
+/*
+Esercizio di oggi: Vue Email List
+nome repo: vue-email-list
+Descrizione:
+Attraverso l'apposita API di Boolean :partying_face:
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+*/
+
 const { createApp } = Vue;
 const app = createApp({
     data() {
         return {
-
+            options:{
+                params:{
+                    min:0,
+                    max:0,
+                    items:0
+                }
+            },
+            elencoMail:[]
         }
     },
 
@@ -12,7 +30,12 @@ const app = createApp({
     },
 
     methods: {
-
+        callApi(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail', this.options).then((res) => {
+                console.log(res.data.response);
+                this.elencoMail = res.data.response;
+            })
+        }
 
     },
 
